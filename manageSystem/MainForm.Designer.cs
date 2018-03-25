f97b3ca0e9f9ab6b582bcc50d309cbdd5a7777e6
Jamos1992 <fsr_123@163.com>
2018-03-25 21:23:52 +0800

    2018-3-25

---
+++ manageSystem/MainForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace manageSystem
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(289, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 31);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Desoutter工具管理系统";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(516, 379);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(185, 63);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "维修保养管理";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(183, 379);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(185, 63);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "工具KPI";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(516, 270);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(185, 63);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "备件管理";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(183, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(185, 63);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "标定管理";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(516, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 63);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "工具信息查询、导出";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(183, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 63);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "工具信息录入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 517);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Desoutter工具管理系统";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ manageSystem/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace manageSystem
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InfoInputForm iif = new InfoInputForm();
+            this.Hide();
+            if (iif.ShowDialog() == DialogResult.OK)
+            {
+                this.Show();
+            }
+        }
+    }
+}

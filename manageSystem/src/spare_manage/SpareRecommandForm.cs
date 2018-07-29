10eb52ed02afa62493868a217a98196a1d1b31e3
Jamos1992 <fsr_123@163.com>
2018-07-29 22:35:17 +0800

    2018-7-29

--- manageSystem/src/spare_manage/SpareRecommandForm.cs
+++ manageSystem/src/spare_manage/SpareRecommandForm.cs
@@ -15,6 +15,7 @@ namespace manageSystem.src.spare_manage
     {
         private RepoSpareToolManage repoSpareToolManage = new RepoSpareToolManage();
         private MaintainInfoManage maintainInfoManage = new MaintainInfoManage();
+        private ToolsInfoManage toolsInfoManage = new ToolsInfoManage();
         public SpareRecommandForm()
         {
             InitializeComponent();
@@ -24,15 +25,39 @@ namespace manageSystem.src.spare_manage
         private void SpareRecommandForm_Load(object sender, EventArgs e)
         {
             List<SpareToolUseHistory> list = repoSpareToolManage.GetSpareToolUseHistoryByDays(90);
-            if (list == null) return;
-            dataVSeaon.DataSource = list;
+            if (list != null) dataVSeaon.DataSource = list;
 
-            maintainInfoManage.GetBreakToolBySql("")
+            List<HalfYear> halfYears = getTopThreeRepair();
+            if (halfYears != null) dataVHalfYear.DataSource = list;
         }
 
         private List<HalfYear> getTopThreeRepair()
         {
-             
+            List<HalfYear> halfYearList = new List<HalfYear>();
+            List<OutputStruct> outputStructs = maintainInfoManage.GetBreakToolBySql($"select distinct ToolModeName from RepairHistory");
+            if (outputStructs != null)
+            {
+                foreach (OutputStruct o in outputStructs)
+                {
+                    int total = 0;
+                    int breakNum = 0;
+                    double partion = 0;
+                    List<ToolsInfo> toolsInfos = toolsInfoManage.QueryToolsInfoBySql($"select * from ToolsInfo where Model='{o.ToolModeName}'");
+                    if (toolsInfos != null) total = toolsInfos.Count;
+
+                    List<OutputStruct> outs = maintainInfoManage.GetBreakToolBySql($"select * from RepairHistory where ToolModeName='{o.ToolModeName}'");
+                    if (outs != null) breakNum = outs.Count;
+
+                    if (total > 0) partion = breakNum / total;
+                    halfYearList.Add(new HalfYear
+                    {
+                        ModelName = o.ToolModeName,
+                        partion = partion.ToString()
+                    });
+                }
+            }
+            halfYearList.Sort();
+            return halfYearList;
         }
 
 
@@ -41,6 +66,6 @@ namespace manageSystem.src.spare_manage
     public class HalfYear
     {
         public string ModelName { get; set; }               //型号
-        public string partion { get; set; }                    //比例
+        public string partion { get; set; }                 //比例
     }
 }
